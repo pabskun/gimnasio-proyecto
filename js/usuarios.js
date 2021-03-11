a9521c27b78fcb99248d36db2a32aea319766473
Pabs Monestel <pablo.monestel@gmail.com>
2021-03-10 19:05:27 -0600

    Modificar usuario
    Creación de página y redirección del modificar usuario

--- js/usuarios.js
+++ js/usuarios.js
@@ -10,7 +10,26 @@ const mostrarTabla = () => {
         fila.insertCell().innerHTML = usuario.nacimiento;
         fila.insertCell().innerHTML = usuario.sexo;
         fila.insertCell().innerHTML = usuario.tipo;
+
+        let celdaAcciones = fila.insertCell();
+
+        let botonModificar = document.createElement('button');
+        botonModificar.innerText = 'Editar';
+        botonModificar.addEventListener('click', () => {
+            sessionStorage.setItem('usuarioSeleccionado', JSON.stringify(usuario));
+            window.location.href = 'usuario-modificar.html';
+        });
+
+
+        let botonEliminar = document.createElement('button');
+        botonEliminar.innerText = 'Eliminar';
+        // Agregarle los botones a la celda
+
+        celdaAcciones.appendChild(botonModificar);
+        celdaAcciones.appendChild(botonEliminar);
+
     });
 };
+
 
 mostrarTabla();
